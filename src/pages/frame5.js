383c3cca3711c52f93a832baf54021d65626c633
Surya Pratap <sps64044@gmail.com>
2025-06-12 19:58:51 +0530

    start-work

--- src/pages/frame5.js
+++ src/pages/frame5.js
@@ -3,7 +3,7 @@ import React from 'react'
 const  Frame5=() =>{
   return (
     <div>
-        <h1 className='text-5xl text-black'>frame5</h1>
+        <h1 className='text-4xl text-black'>frame5</h1>
       
     </div>
   )
